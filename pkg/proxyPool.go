0780d24a92eff17cc5c04f79a79e6f92a9004c9d
kdj <59144168+ckaidi@users.noreply.github.com>
2024-03-01 00:29:41 +0800

    new

--- pkg/proxyPool.go
+++ pkg/proxyPool.go
@@ -1,1 +1,17 @@
 package pkg
+
+type Proxy struct {
+	Anonymous  string `json:"anonymous"`
+	CheckCount string `json:"check_count"`
+	FailCount  string `json:"fail_count"`
+	Https      string `json:"https"`
+	LastStatus string `json:"last_status"`
+	LastTime   string `json:"last_time"`
+	Addr       string `json:"proxy"`
+	Region     string `json:"region"`
+	Source     string `json:"source"`
+}
+
+func GetProxy() {
+
+}
