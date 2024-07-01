a6fee6d528ab68e7c50925d53de4aff0132299f6
kdj <59144168+ckaidi@users.noreply.github.com>
2024-07-01 14:14:13 +0800

    渐进式转ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     vue(),
   ],
   resolve: {
+    extensions:['.js','.vue','.json','.ts'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
